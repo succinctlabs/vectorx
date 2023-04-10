8b69d52c6df950f3ec8d546c73ad66d90f591033
Kevin Jue <kjue235@gmail.com>
2023-04-10 16:42:38 -0700

    used different rpc apis

--- src/offchain_actors/grandpa_justif_proof_generator.rs
+++ src/offchain_actors/grandpa_justif_proof_generator.rs
@@ -1,73 +1,47 @@
-use std::sync::Arc;
+
 use std::time::SystemTime;
 
+use avail_proof_generators::gadgets::consensus::{GrandpaJustificationVerifierTargets, build_grandpa_justification_verifier};
 use avail_subxt::{api, build_client, primitives::Header};
 use codec::{Decode, Encode};
-use ed25519::gadgets::nonnative::WitnessNonNative;
+use ::ed25519::curve::ed25519::Ed25519;
+use ::ed25519::curve::eddsa::{EDDSASignature, verify_message, EDDSAPublicKey};
+use ::ed25519::field::ed25519_scalar::Ed25519Scalar;
+use ::ed25519::gadgets::curve::{decompress_point, WitnessAffinePoint};
+use ::ed25519::gadgets::nonnative::WitnessNonNative;
+use num::BigUint;
 use plonky2::iop::witness::{PartialWitness, Witness};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
-use plonky2::plonk::circuit_data::{CircuitConfig, CircuitData};
+use plonky2::plonk::circuit_data::{CircuitData, CircuitConfig};
 use plonky2::plonk::config::{PoseidonGoldilocksConfig, GenericConfig};
-use avail_proof_generators::gadgets::consensus::{build_grandpa_justification_verifier, GrandpaJustificationVerifierTargets};
 use plonky2::plonk::proof::ProofWithPublicInputs;
 use plonky2_field::goldilocks_field::GoldilocksField;
 use plonky2_field::types::Field;
-use serde::{Deserialize, Serialize};
-use sp_core::blake2_256;
+use serde::de::Error;
+use serde::Deserialize;
 use sp_core::{
+    blake2_256, bytes,
     ed25519::{Public as EdPublic, Signature},
     H256,
 };
 use subxt::rpc::RpcParams;
-use jsonrpsee::ws_client::WsClientBuilder;
-use ::ed25519::curve::ed25519::Ed25519;
-use ed25519::gadgets::curve::{decompress_point, WitnessAffinePoint};
-use ed25519::curve::eddsa::{verify_message, EDDSAPublicKey, EDDSASignature};
-use ed25519::field::ed25519_scalar::Ed25519Scalar;
-use num::BigUint;
 
 const D: usize = 2;
 type C = PoseidonGoldilocksConfig;
 type F = <C as GenericConfig<D>>::F;
 type Curve = Ed25519;
 
-#[derive(Clone, Serialize, Deserialize)]
-pub struct JustificationNotification(pub sp_core::Bytes);
-
-// Perform a highly unsafe type-casting between two types hidden behind an Arc.
-pub unsafe fn unsafe_arc_cast<T, U>(arc: Arc<T>) -> Arc<U> {
-	let ptr = Arc::into_raw(arc).cast::<U>();
-	Arc::from_raw(ptr)
+#[derive(Deserialize, Debug)]
+pub struct SubscriptionMessageResult {
+    pub result: String,
+    pub subscription: String,
 }
 
-// We redefine these here because we want the header to be bounded by subxt::config::Header in the
-// prover
-/// Commit
-pub type Commit = finality_grandpa::Commit<H256, u32, Signature, EdPublic>;
-
-/// Justification
-#[cfg_attr(any(feature = "std", test), derive(Debug))]
-#[derive(Clone, Encode, Decode)]
-pub struct GrandpaJustification {
-	/// Current voting round number, monotonically increasing
-	pub round: u64,
-	/// Contains block hash & number that's being finalized and the signatures.
-	pub commit: Commit,
-	/// Contains the path from a [`PreCommit`]'s target hash to the GHOST finalized block.
-	pub votes_ancestries: Vec<Header>,
-}
-
-/// Finality for block B is proved by providing:
-/// 1) the justification for the descendant block F;
-/// 2) headers sub-chain (B; F] if B != F;
-#[derive(Debug, PartialEq, Encode, Decode, Clone)]
-pub struct FinalityProof<H: codec::Codec> {
-	/// The hash of block F for which justification is provided.
-	pub block: H256,
-	/// Justification of the block F.
-	pub justification: Vec<u8>,
-	/// The set of headers in the range (B; F] that we believe are unknown to the caller. Ordered.
-	pub unknown_headers: Vec<H>,
+#[derive(Deserialize, Debug)]
+pub struct SubscriptionMessage {
+    pub jsonrpc: String,
+    pub params: SubscriptionMessageResult,
+    pub method: String,
 }
 
 #[derive(Clone, Debug, Decode, Encode, Deserialize)]
@@ -75,6 +49,50 @@ pub struct Precommit {
     pub target_hash: H256,
     /// The target block's number
     pub target_number: u32,
+}
+
+#[derive(Clone, Debug, Decode, Deserialize)]
+pub struct SignedPrecommit {
+    pub precommit: Precommit,
+    /// The signature on the message.
+    pub signature: Signature,
+    /// The Id of the signer.
+    pub id: EdPublic,
+}
+#[derive(Clone, Debug, Decode, Deserialize)]
+pub struct Commit {
+    pub target_hash: H256,
+    /// The target block's number.
+    pub target_number: u32,
+    /// Precommits for target block or any block after it that justify this commit.
+    pub precommits: Vec<SignedPrecommit>,
+}
+
+#[derive(Clone, Debug, Decode)]
+pub struct GrandpaJustification {
+    pub round: u64,
+    pub commit: Commit,
+    pub votes_ancestries: Vec<Header>,
+}
+
+impl<'de> Deserialize<'de> for GrandpaJustification {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let encoded = bytes::deserialize(deserializer)?;
+        Self::decode(&mut &encoded[..])
+            .map_err(|codec_err| D::Error::custom(format!("Invalid decoding: {:?}", codec_err)))
+    }
+}
+
+#[derive(Debug, Decode)]
+pub struct Authority(EdPublic, u64);
+
+#[derive(Debug, Encode)]
+pub enum SignerMessage {
+    DummyMessage(u32),
+    PrecommitMessage(Precommit),
 }
 
 pub const CHUNK_128_BYTES: usize = 128;
@@ -164,13 +182,14 @@ pub async fn main() {
     let grandpa_justif_circuit = builder.build::<C>();
 
     let url: &str = "wss://testnet.avail.tools:443/ws";
+    
     let c = build_client(url).await.unwrap();
     let t = c.rpc();
-    let sub: Result<subxt::rpc::Subscription<Header>, subxt::Error> = t
+    let sub: Result<subxt::rpc::Subscription<GrandpaJustification>, subxt::Error> = t
         .subscribe(
-            "chain_subscribeFinalizedHeads",
+            "grandpa_subscribeJustifications",
             RpcParams::new(),
-            "chain_unsubscribeFinalizedHeads",
+            "grandpa_unsubscribeJustifications",
         )
         .await;
 
@@ -180,44 +199,35 @@ pub async fn main() {
     const FINALIZATION_PERIOD: usize = 5;
 
     // Wait for headers
-    while let Some(Ok(header)) = sub.next().await {
-        println!("Got header: {:?}", header.number);
+    while let Some(Ok(justification)) = sub.next().await {
+        // Get the header corresponding to the new justification
+        let header = c
+            .rpc()
+            .header(Some(justification.commit.target_hash))
+            .await
+            .unwrap()
+            .unwrap();
+
         if header.number % (FINALIZATION_PERIOD as u32) == 0 {
-            println!("Going to retrieve the justification for header: {:?}", header.number);
-            let encoded_header = header.encode();
+            let block_hash: H256 = Encode::using_encoded(&header, blake2_256).into();
+            println!("Going to generate justification for header with number: {:?} and hash: {:?}", header.number, block_hash);
 
-            let relay_ws_client = Arc::new(WsClientBuilder::default().build(url).await);
-            let encoded_justification = finality_grandpa_rpc::GrandpaApiClient::<JustificationNotification, H256, u32>::prove_finality(
-                &*unsafe {
-					unsafe_arc_cast::<_, jsonrpsee_ws_client::WsClient>(
-						relay_ws_client
-					)
-				},
-                header.number
-            ).await.unwrap().unwrap().0;
-
-            let mut finality_proof = FinalityProof::<H256>::decode(&mut &encoded_justification[..]).unwrap();
-
-            let justification: GrandpaJustification =
-                GrandpaJustification::decode(&mut &finality_proof.justification[..]).unwrap();
-
+            // Get current authority set ID
             let set_id_key = api::storage().grandpa().current_set_id();
             let set_id = c.storage().fetch(&set_id_key, None).await.unwrap().unwrap();
 
             // Form a message which is signed in the justification
             let encoded_message = Encode::encode(&(
-                &Precommit{ target_hash: Encode::using_encoded(&header, blake2_256).into(), target_number: header.number }, 
+                &SignerMessage::PrecommitMessage(justification.commit.precommits[0].clone().precommit),
                 &justification.round,
                 &set_id,
             ));
 
             // retrieve the signatures
-            let mut signatures: Vec<&Signature> = Vec::new();
-            let mut pub_keys: Vec<EdPublic> = Vec::new();
-            for i in 0..justification.commit.precommits.len() {
-                signatures.push(&justification.commit.precommits[i].signature);
-                pub_keys.push(justification.commit.precommits[i].id);
-            }
+            let signatures = justification.commit.precommits.iter().map(|x| &x.signature).collect::<Vec<&Signature>>();
+            let pub_keys = justification.commit.precommits.iter().map(|x| x.id).collect::<Vec<EdPublic>>();
+
+            let encoded_header = header.encode();
 
             /*
             println!("encoded_header is {:?}", encoded_header);

9a3182b38990affd05779c0014a293f83b0fbc6e
Kevin Jue <kjue235@gmail.com>
2023-08-18 18:42:42 -0700

    initial code for header verifiation ivc

--- circuits/src/header_verification.rs
+++ circuits/src/header_verification.rs
@@ -1,11 +1,26 @@
-use plonky2::{iop::target::Target, hash::hash_types::RichField, field::extension::Extendable, plonk::circuit_builder::CircuitBuilder};
-use plonky2x::hash::blake2::blake2b::blake2b;
+use itertools::Itertools;
+use plonky2::{iop::target::{Target, BoolTarget}, hash::hash_types::{RichField, HashOutTarget, NUM_HASH_OUT_ELTS}, field::{extension::Extendable, goldilocks_field}, plonk::{circuit_builder::CircuitBuilder, circuit_data::{CommonCircuitData, CircuitConfig}, proof::ProofWithPublicInputsTarget, config::{GenericConfig, AlgebraicHasher}}, gates::{gate::GateRef, noop::NoopGate, constant::ConstantGate, poseidon_mds::PoseidonMdsGate, public_input::PublicInputGate, base_sum::BaseSumGate, reducing_extension::ReducingExtensionGate, reducing::ReducingGate, arithmetic_extension::ArithmeticExtensionGate, arithmetic_base::ArithmeticGate, multiplication_extension::MulExtensionGate, random_access::RandomAccessGate, exponentiation::ExponentiationGate, coset_interpolation::CosetInterpolationGate, poseidon::PoseidonGate}, fri::{FriParams, FriConfig, reduction_strategies::FriReductionStrategy}};
+use plonky2::gates::selectors::SelectorsInfo;
+use plonky2x::{hash::blake2::blake2b::blake2b, num::u32::gates::add_many_u32::U32AddManyGate};
 
 use crate::{utils::{AvailHashTarget, CHUNK_128_BYTES, HASH_SIZE, EncodedHeaderTarget, CircuitBuilderUtils}, decoder::CircuitBuilderHeaderDecoder};
 
 pub const MAX_HEADER_SIZE: usize = CHUNK_128_BYTES * 16; // 2048 bytes
 
-pub trait CircuitBuilderHeaderVerification {
+struct PublicInputsElements {
+    initial_block_hash: AvailHashTarget,
+    initial_block_num: Target,
+    initial_data_root_accumulator: AvailHashTarget,
+    latest_block_hash: AvailHashTarget,
+    latest_block_num: Target,
+    latest_data_root_accumulator: AvailHashTarget,
+    previous_circuit_digest: HashOutTarget,
+    previous_constants_sigmas_cap: HashOutTarget,
+    circuit_digest: HashOutTarget,
+    constants_sigmas_cap: HashOutTarget,
+}
+
+pub trait CircuitBuilderHeaderVerification<F: RichField + Extendable<D>, const D: usize> {
     fn verify_header(
         &mut self,
         encoded_header: &EncodedHeaderTarget,
@@ -14,9 +29,17 @@ pub trait CircuitBuilderHeaderVerification {
         parent_hash: &AvailHashTarget,
         data_root_acc: &AvailHashTarget,
     );
+
+    fn verify_header_ivc<C: GenericConfig<D, F = F>>(&mut self) -> (BoolTarget, ProofWithPublicInputsTarget<D>)
+    where
+    C::Hasher: AlgebraicHasher<F>;
+
+    fn header_verification_ivc_common_data(&mut self) -> CommonCircuitData<F, D>;
+
+    fn parse_public_inputs(&mut self, public_inputs: Vec<Target>) -> PublicInputsElements;
 }
 
-impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilderHeaderVerification for CircuitBuilder<F, D> {
+impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilderHeaderVerification<F, D> for CircuitBuilder<F, D> {
     fn verify_header(
         &mut self,
         encoded_header: &EncodedHeaderTarget,
@@ -112,50 +135,74 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilderHeaderVerificat
             self.register_public_input(byte);
         }
     }
-}
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_process_header_ivc() -> Result<()> {
-        const D: usize = 2;
-        type C = PoseidonGoldilocksConfig;
-        type F = <C as GenericConfig<D>>::F;
+    fn verify_header_ivc<C: GenericConfig<D, F = F>>(&mut self) -> (BoolTarget, ProofWithPublicInputsTarget<D>)
+    where
+    C::Hasher: AlgebraicHasher<F>,
+    {
+        // False for the base case, true otherwise
+        let condition = self.add_virtual_bool_target_safe();
 
-        let mut builder_logger = env_logger::Builder::from_default_env();
-        builder_logger.format_timestamp(None);
-        builder_logger.filter_level(log::LevelFilter::Trace);
-        builder_logger.try_init()?;
+        let common_data = self.header_verification_ivc_common_data();
 
-        let config = CircuitConfig::standard_recursion_config();
-        let mut builder = CircuitBuilder::<F, D>::new(config);
+        // Unpack inner proof's public inputs.
+        let previous_header_verification_proof_with_pis = self.add_virtual_proof_with_pis(&common_data);        
+        let previous_proof_elements = self.parse_public_inputs(previous_header_verification_proof_with_pis.public_inputs);
 
-        let head_block_num = builder.add_virtual_public_input();
-        let head_block_hash = builder.add_virtual_avail_hash_target_safe(true);
-        let initial_accumulator = builder.add_virtual_avail_hash_target_safe(true);
+        // We need to ensure that all of the IVC iterations are using the same
+        // circuit digest and constants_sigmas_cap.
+        // These values are known after the circuit is built, so the 
+        // verifier of the final proof will need to check that these values 
+        // match the built circuit's values.
+        self.connect_hashes(previous_proof_elements.previous_circuit_digest, previous_proof_elements.circuit_digest);
+        self.connect_hashes(previous_proof_elements.previous_constants_sigmas_cap, previous_proof_elements.constants_sigmas_cap);
 
-        let current_block_num = builder.add_virtual_target();
-        let current_block_hash = builder.add_virtual_avail_hash_target_safe(false);
-        let current_accumulator = builder.add_virtual_avail_hash_target_safe(false);
+        // Create inputs for the current encoded block;
+        let encoded_block_input = self.add_virtual_encoded_header_target_safe();
+        let encoded_block_size = self.add_virtual_target();
 
-        let encoded_block_input = builder.add_virtual_encoded_header_target_safe();
-        let encoded_block_size = builder.add_virtual_target();
+        let previous_block_hash = self.random_access_avail_hash(
+            condition.target, 
+            vec![previous_proof_elements.initial_block_hash, previous_proof_elements.latest_block_hash]);
 
-        builder.process_header(
+        let previous_block_num = self.random_access(
+            condition.target, 
+            vec![previous_proof_elements.initial_block_num, previous_proof_elements.latest_block_num]);
+
+        let previous_data_root_accumulator = self.random_access_avail_hash(
+            condition.target,
+            vec![previous_proof_elements.initial_data_root_accumulator, previous_proof_elements.latest_data_root_accumulator]);
+
+        self.verify_header(
             &encoded_block_input,
             encoded_block_size,
-            current_block_num,
-            &current_block_hash,
-            &current_accumulator,
+            previous_block_num,
+            &previous_block_hash,
+            &previous_data_root_accumulator,
         );
 
-        let verifier_data_target = builder.add_verifier_data_public_inputs();
+        // Register the previous proof's circuit digest and constants sigma cap as public input
+        self.register_public_inputs(&previous_proof_elements.circuit_digest.elements);
+        self.register_public_inputs(&previous_proof_elements.constants_sigmas_cap.elements);
 
+        self.conditionally_verify_cyclic_proof_or_dummy::<C>(
+            condition,
+            &previous_header_verification_proof_with_pis,
+            &common_data,
+        );
+
+        return (
+            condition,
+            previous_header_verification_proof_with_pis
+        )
+    }
+
+    fn header_verification_ivc_common_data(&mut self) -> CommonCircuitData<F, D> {
         let k_is = vec![1, 7, 49, 343, 2401, 16807, 117649, 823543, 5764801, 40353607, 282475249, 1977326743, 13841287201, 96889010407, 678223072849, 4747561509943, 33232930569601, 232630513987207, 1628413597910449, 11398895185373143, 79792266297612001, 558545864083284007, 3909821048582988049, 8922003270666332022, 7113790686420571191, 12903046666114829695, 16534350385145470581, 5059988279530788141, 16973173887300932666, 8131752794619022736, 1582037354089406189, 11074261478625843323, 3732854072722565977, 7683234439643377518, 16889152938674473984, 7543606154233811962, 15911754940807515092, 701820169165099718, 4912741184155698026, 15942444219675301861, 916645121239607101, 6416515848677249707, 8022122801911579307, 814627405137302186, 5702391835961115302, 3023254712898638472, 2716038920875884983, 565528376716610560, 3958698637016273920, 9264146389699333119, 9508792519651578870, 11221315429317299127, 4762231727562756605, 14888878023524711914, 11988425817600061793, 10132004445542095267, 15583798910550913906, 16852872026783475737, 7289639770996824233, 14133990258148600989, 6704211459967285318, 10035992080941828584, 14911712358349047125, 12148266161370408270, 11250886851934520606, 4969231685883306958, 16337877731768564385, 3684679705892444769, 7346013871832529062, 14528608963998534792, 9466542400916821939, 10925564598174000610, 2691975909559666986, 397087297503084581, 2779611082521592067, 1010533508236560148, 7073734557655921036, 12622653764762278610, 14571600075677612986, 9767480182670369297];
-        let k_i_fields = k_is.iter().map(|x| plonky2_field::goldilocks_field::GoldilocksField(*x)).collect::<Vec<_>>();
+        let k_i_fields = k_is.iter().map(|x| goldilocks_field::GoldilocksField(*x)).collect::<Vec<_>>();
 
         let barycentric_weights = vec![17293822565076172801,18374686475376656385,18446744069413535745,281474976645120,17592186044416,18446744069414584577,18446744000695107601,18446744065119617025,1152921504338411520,72057594037927936,18446744069415632897,18446462594437939201,18446726477228539905,18446744069414584065,68719476720,4294967296];
-        let barycentric_weights_fields = barycentric_weights.iter().map(|x| plonky2_field::goldilocks_field::GoldilocksField(*x)).collect::<Vec<_>>();
+        let barycentric_weights_fields = barycentric_weights.iter().map(|x| goldilocks_field::GoldilocksField(*x)).collect::<Vec<_>>();
 
         let common_data = CommonCircuitData::<F, D> {
             config: CircuitConfig {
@@ -216,43 +263,57 @@ mod tests {
             num_public_inputs: 198,
             k_is: k_i_fields,
             num_partial_products: 9,
+            num_lookup_polys: 0,
+            num_lookup_selectors: 0,
+            luts: Vec::new(),
         };
 
-        let condition = builder.add_virtual_bool_target_safe();
+        common_data
+    }
+    
+    fn parse_public_inputs(&mut self, public_inputs: Vec<Target>) -> PublicInputsElements {
+        assert!(public_inputs.len() == 2 * (HASH_SIZE + 1 + HASH_SIZE) + 4 * NUM_HASH_OUT_ELTS);
 
-        // Unpack inner proof's public inputs.
-        let inner_cyclic_proof_with_pis = builder.add_virtual_proof_with_pis(&common_data);
-        let inner_cyclic_pis = &inner_cyclic_proof_with_pis.public_inputs;
-        let inner_cyclic_initial_block_num = inner_cyclic_pis[0];
-        let inner_cyclic_initial_block_hash = AvailHashTarget(inner_cyclic_pis[1..33].try_into().unwrap());
-        let inner_cyclic_initial_accumulator = AvailHashTarget(inner_cyclic_pis[33..65].try_into().unwrap());
-        let inner_cyclic_latest_block_num = inner_cyclic_pis[65];
-        let inner_cyclic_latest_block_hash = AvailHashTarget(inner_cyclic_pis[66..98].try_into().unwrap());
-        let inner_cyclic_latest_accumulator = AvailHashTarget(inner_cyclic_pis[98..130].try_into().unwrap());
+        let public_inputs_iter = public_inputs.into_iter();
 
-        // Connect our initial values to that of our inner proof. (If there is no inner proof, the
-        // initial values will be unconstrained, which is intentional.)
-        builder.connect(head_block_num, inner_cyclic_initial_block_num);
-        builder.connect_hash(head_block_hash.clone(), inner_cyclic_initial_block_hash);
-        builder.connect_hash(initial_accumulator.clone(), inner_cyclic_initial_accumulator);
+        PublicInputsElements { 
+            initial_block_hash: AvailHashTarget(public_inputs_iter.take(HASH_SIZE).collect_vec().as_slice().try_into().unwrap()),
+            initial_block_num: public_inputs_iter.take(1).collect_vec()[0],
+            initial_data_root_accumulator: AvailHashTarget(public_inputs_iter.take(HASH_SIZE).collect_vec().as_slice().try_into().unwrap()),
+            latest_block_hash: AvailHashTarget(public_inputs_iter.take(HASH_SIZE).collect_vec().as_slice().try_into().unwrap()),
+            latest_block_num:  public_inputs_iter.take(1).collect_vec()[0],
+            latest_data_root_accumulator: AvailHashTarget(public_inputs_iter.take(HASH_SIZE).collect_vec().as_slice().try_into().unwrap()),
+            previous_circuit_digest: HashOutTarget { elements: public_inputs_iter.take(NUM_HASH_OUT_ELTS).collect_vec().as_slice().try_into().unwrap() },
+            previous_constants_sigmas_cap: HashOutTarget { elements: public_inputs_iter.take(NUM_HASH_OUT_ELTS).collect_vec().as_slice().try_into().unwrap() },
+            circuit_digest: HashOutTarget { elements: public_inputs_iter.take(NUM_HASH_OUT_ELTS).collect_vec().as_slice().try_into().unwrap() },
+            constants_sigmas_cap: HashOutTarget { elements: public_inputs_iter.take(NUM_HASH_OUT_ELTS).collect_vec().as_slice().try_into().unwrap() },
+        }
+    }
 
-        // The input values is the previous outputs if we have an inner proof, or the initial values
-        // if this is the base case.
-        let actual_block_num_in =
-            builder.select(condition, inner_cyclic_latest_block_num, head_block_num);
-        let actual_block_hash_in =
-            builder.select_hash(condition, &inner_cyclic_latest_block_hash, &head_block_hash);
-        let actual_accumulator_in =
-            builder.select_hash(condition, &inner_cyclic_latest_accumulator, &initial_accumulator);
-        builder.connect(current_block_num, actual_block_num_in);
-        builder.connect_hash(current_block_hash, actual_block_hash_in);
-        builder.connect_hash(current_accumulator, actual_accumulator_in);
+}
 
-        builder.conditionally_verify_cyclic_proof_or_dummy::<C>(
-            condition,
-            &inner_cyclic_proof_with_pis,
-            &common_data,
-        )?;
+
+#[cfg(test)]
+mod tests {
+    use plonky2::{plonk::{config::{PoseidonGoldilocksConfig, GenericConfig}, circuit_data::{CircuitConfig, CommonCircuitData}, circuit_builder::CircuitBuilder}, recursion::cyclic_recursion::check_cyclic_proof_verifier_data, iop::witness::PartialWitness};
+
+    use crate::utils::tests::{BLOCK_530508_HEADER, BLOCK_530509_HEADER, BLOCK_530510_HEADER, BLOCK_530508_PARENT_HASH};
+
+    #[test]
+    fn test_verify_headers_ivc() -> Result<()> {
+        const D: usize = 2;
+        type C = PoseidonGoldilocksConfig;
+        type F = <C as GenericConfig<D>>::F;
+
+        let mut builder_logger = env_logger::Builder::from_default_env();
+        builder_logger.format_timestamp(None);
+        builder_logger.filter_level(log::LevelFilter::Trace);
+        builder_logger.try_init()?;
+
+        let config = CircuitConfig::standard_recursion_config();
+        let mut builder = CircuitBuilder::<F, D>::new(config);
+
+        builder.verify_header_ivc();
 
         println!("KJ: building circuit");
         let cyclic_circuit_data = builder.build::<C>();
@@ -262,7 +323,7 @@ mod tests {
             BLOCK_530508_HEADER.to_vec(),
             BLOCK_530509_HEADER.to_vec(),
             BLOCK_530510_HEADER.to_vec(),
-            BLOCK_530511_HEADER.to_vec(),
+            /*BLOCK_530511_HEADER.to_vec(),
             BLOCK_530512_HEADER.to_vec(),
             BLOCK_530513_HEADER.to_vec(),
             BLOCK_530514_HEADER.to_vec(),
@@ -278,11 +339,11 @@ mod tests {
             BLOCK_530524_HEADER.to_vec(),
             BLOCK_530525_HEADER.to_vec(),
             BLOCK_530526_HEADER.to_vec(),
-            BLOCK_530527_HEADER.to_vec(),
+            BLOCK_530527_HEADER.to_vec(),*/
         ];
-        let head_block_hash_val = hex::decode(BLOCK_530508_PARENT_HASH).unwrap();
-        let head_block_num_val = 530507;
-        let initial_accumulator_val = [0u8; 32];
+        let initial_block_hash_val = hex::decode(BLOCK_530508_PARENT_HASH).unwrap();
+        let initial_block_num_val = 530507;
+        let initial_data_root_accumulator_val = [1u8; 32];
 
         let mut pw = PartialWitness::new();
         pw.set_bool_target(condition, false);

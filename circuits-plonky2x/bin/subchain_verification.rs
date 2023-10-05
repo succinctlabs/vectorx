f3a0cffa6d3afeee31cdb76ac55a8495cc643129
Kevin Jue <kjue235@gmail.com>
2023-10-04 18:28:19 -0700

    more cleanup

--- circuits-plonky2x/bin/subchain_verification.rs
+++ circuits-plonky2x/bin/subchain_verification.rs
@@ -4,7 +4,3 @@ use plonky2x::backend::function::VerifiableFunction;
 pub fn main() {
     VerifiableFunction::<SubchainVerificationMRCircuit>::entrypoint();
 }
-
-// RELEASE_ID=1ff12645-3f2a-459b-ad08-3e74e62c4075 PROVER=remote PROOF_SERVICE_URL=https://alpha.succinct.xyz cargo run --release --bin subchain_verification prove --input-json input.json
-
-// RELEASE_ID=1ff12645-3f2a-459b-ad08-3e74e62c4075 PROVER=remote PROOF_SERVICE_URL=https://alpha.succinct.xyz cargo test --package avail_plonky2x --lib --release -- subchain_verification_map_reduce::tests --nocapture

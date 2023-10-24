b229b2129dd443d79d3fa44c85060cd1e2059cfe
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-10-24 16:22:21 -0700

    refactor verify_epoch_end_header to rm some select array calls

--- circuits-plonky2x/src/builder/rotate.rs
+++ circuits-plonky2x/src/builder/rotate.rs
@@ -41,61 +41,8 @@ impl<L: PlonkParameters<D>, const D: usize> RotateMethods for CircuitBuilder<L, 
         expected_new_authority_set_hash: &Bytes32Variable,
     ) {
         let header_bytes = &header.header_bytes;
-        let one = self.one();
-
-        let mut cursor = *start_position;
-
-        // Skip 1 byte, TODO: Figure out what this byte is.
-        cursor = self.add(cursor, one);
-
-        // Verify the next byte is 0x04 (Consensus Flag = 4u32).
-        let consensus_enum_flag = self.constant::<ByteVariable>(4u8);
-        // TODO: This is inefficient, see if there's a better way to do this.
-        let header_consensus_flag = self.select_array(&header_bytes.data, cursor);
-        self.assert_is_equal(header_consensus_flag, consensus_enum_flag);
-        cursor = self.add(cursor, one);
-
-        // Verify the next 4 bytes are the Consensus Engine ID: 0x46524e4b [70, 82, 78, 75].
-        // TODO: Link to the Consensus Engine ID in subxt for Grandpa.
-        let consensus_id_bytes =
-            self.constant::<ArrayVariable<ByteVariable, 4>>([70u8, 82u8, 78u8, 75u8].to_vec());
-        for i in 0..4 {
-            // TODO: select_array is inefficient, see if there's a better way to do this.
-            let header_consensus_id_byte = self.select_array(&header_bytes.data, cursor);
-            self.assert_is_equal(header_consensus_id_byte, consensus_id_bytes[i]);
-            cursor = self.add(cursor, one);
-        }
-
-        // Skip 2 bytes
-        // TODO: Validate what the 2 bytes are. Not sure if this is ncessary.
-        for _ in 0..2 {
-            cursor = self.add(cursor, one);
-        }
-
-        // Verify the next byte is 0x01, denoting a ScheduledChange.
-        let scheduled_change_enum_flag = self.constant::<ByteVariable>(1u8);
-        // TODO: select_array is inefficient, see if there's a better way to do this.
-        let header_schedule_change_flag = self.select_array(&header_bytes.data, cursor);
-        self.assert_is_equal(header_schedule_change_flag, scheduled_change_enum_flag);
-        cursor = self.add(cursor, one);
-
-        // Skip 2 bytes
-        // TODO: Validate what the 2 bytes are. Not sure if this is ncessary.
-        for _ in 0..2 {
-            cursor = self.add(cursor, one);
-        }
-
-        let pubkey_len = self.constant::<Variable>(L::Field::from_canonical_usize(PUBKEY_LENGTH));
-        let weight_len = self.constant::<Variable>(L::Field::from_canonical_usize(WEIGHT_LENGTH));
-        let delay_len = self.constant::<Variable>(L::Field::from_canonical_usize(DELAY_LENGTH));
-
-        let weight_bytes = self.constant::<ArrayVariable<ByteVariable, WEIGHT_LENGTH>>(
-            [1u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8].to_vec(),
-        );
-
         // Convert header to Variable from ByteVariable for get_fixed_subarray.
-        let header_variables = header
-            .header_bytes
+        let header_variables = header_bytes
             .as_vec()
             .iter()
             .map(|x: &ByteVariable| x.to_variable(self))
@@ -103,11 +50,13 @@ impl<L: PlonkParameters<D>, const D: usize> RotateMethods for CircuitBuilder<L, 
         let header_as_variables =
             ArrayVariable::<Variable, MAX_HEADER_SIZE>::from(header_variables);
 
-        let true_v = self._true();
-        let mut validator_enabled = self._true();
+        let one = self.one();
+        let pubkey_len = self.constant::<Variable>(L::Field::from_canonical_usize(PUBKEY_LENGTH));
+        let weight_len = self.constant::<Variable>(L::Field::from_canonical_usize(WEIGHT_LENGTH));
+        let delay_len = self.constant::<Variable>(L::Field::from_canonical_usize(DELAY_LENGTH));
+        let consensus_engine_id_len = self.constant::<Variable>(L::Field::from_canonical_usize(4));
 
-        let delay_bytes =
-            self.constant::<ArrayVariable<ByteVariable, 4>>([0u8, 0u8, 0u8, 0u8].to_vec());
+        let mut cursor = *start_position;
 
         // Note: Use header_hash as seed for randomness.
         // Get the maximum size subarray of the header bytes that we want to verify.
@@ -124,9 +73,59 @@ impl<L: PlonkParameters<D>, const D: usize> RotateMethods for CircuitBuilder<L, 
                 .collect::<Vec<_>>(),
         );
 
+        // Skip 1 byte, TODO: Figure out what this byte is.
+        cursor = self.add(cursor, one);
+
+        // Verify the next byte is 0x04 (Consensus Flag = 4u32).
+        let consensus_enum_flag = self.constant::<ByteVariable>(4u8);
+        let header_consensus_flag = subarray[1];
+        self.assert_is_equal(header_consensus_flag, consensus_enum_flag);
+        cursor = self.add(cursor, one);
+
+        // Verify the next 4 bytes are the Consensus Engine ID: 0x46524e4b [70, 82, 78, 75].
+        // TODO: Link to the Consensus Engine ID in subxt for Grandpa.
+        let consensus_id_bytes =
+            self.constant::<ArrayVariable<ByteVariable, 4>>([70u8, 82u8, 78u8, 75u8].to_vec());
+        self.assert_is_equal(
+            ArrayVariable::<ByteVariable, 4>::from(subarray[2..6].to_vec()),
+            consensus_id_bytes,
+        );
+        cursor = self.add(cursor, consensus_engine_id_len);
+
+        // Skip 2 bytes
+        // TODO: Validate what the 2 bytes are. Not sure if this is ncessary.
+        for _ in 0..2 {
+            cursor = self.add(cursor, one);
+        }
+
+        // Verify the next byte is 0x01, denoting a ScheduledChange.
+        let scheduled_change_enum_flag = self.constant::<ByteVariable>(1u8);
+        let header_schedule_change_flag = subarray[8];
+        self.assert_is_equal(header_schedule_change_flag, scheduled_change_enum_flag);
+        cursor = self.add(cursor, one);
+
+        // Skip 2 bytes
+        // TODO: Validate what the 2 bytes are. Not sure if this is ncessary.
+        for _ in 0..2 {
+            cursor = self.add(cursor, one);
+        }
+
+        let expected_weight_bytes = self.constant::<ArrayVariable<ByteVariable, WEIGHT_LENGTH>>(
+            [1u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8].to_vec(),
+        );
+
+        let true_v = self._true();
+        let mut validator_enabled = self._true();
+
+        let delay_bytes =
+            self.constant::<ArrayVariable<ByteVariable, 4>>([0u8, 0u8, 0u8, 0u8].to_vec());
+
+        // The length of the bytes before the encoded authority set.
+        const PREFIX_LENGTH: usize = 1 + 1 + 4 + 2 + 1 + 2;
+
         // Verify num_authorities validators are present and valid.
         for i in 0..(MAX_AUTHORITY_SET_SIZE) {
-            let idx = i * VALIDATOR_LENGTH;
+            let idx = (i * VALIDATOR_LENGTH) + PREFIX_LENGTH;
             let curr_validator = self.constant::<Variable>(L::Field::from_canonical_usize(i));
 
             let at_delay = self.is_equal(curr_validator, *num_authorities);
@@ -148,7 +147,7 @@ impl<L: PlonkParameters<D>, const D: usize> RotateMethods for CircuitBuilder<L, 
             let weight = ArrayVariable::<ByteVariable, WEIGHT_LENGTH>::from(
                 subarray[idx + PUBKEY_LENGTH..idx + VALIDATOR_LENGTH].to_vec(),
             );
-            let correct_weight = self.is_equal(weight, weight_bytes.clone());
+            let correct_weight = self.is_equal(weight, expected_weight_bytes.clone());
             let is_valid_weight = self.or(validator_disabled, correct_weight);
             self.assert_is_equal(is_valid_weight, true_v);
             // Increment the cursor by the weight length.

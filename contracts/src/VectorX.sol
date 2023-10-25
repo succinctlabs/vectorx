cee2fb1cf253bdedf9a676de3d31d8c08aa4522a
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-10-25 15:58:15 -0700

    initial contract

--- contracts/src/VectorX.sol
+++ contracts/src/VectorX.sol
@@ -14,14 +14,12 @@ contract VectorX {
     mapping(bytes32 => bytes32) public dataRootCommitments;
     mapping(bytes32 => bytes32) public stateRootCommitments;
 
-    uint64 public currentAuthoritySetId;
-    bytes32 public currentAuthoritySetHash;
-
     uint32 public head;
 
     uint32 public constant MAX_RANGE = 128;
 
     bytes32 public headerRangeFunctionId;
+    bytes32 public rotateFunctionId;
 
     event HeaderRangeRequested(
         uint32 trustedBlock,
@@ -37,6 +35,18 @@ contract VectorX {
         bytes32 targetHeaderHash,
         bytes32 dataRootCommitment,
         bytes32 stateRootCommitment
+    );
+
+    event RotateRequested(
+        uint64 currentAuthoritySetId,
+        bytes32 currentAuthoritySetHash,
+        uint64 epochEndBlock
+    );
+
+    event RotateFulfilled(
+        uint64 newAuthoritySetId,
+        bytes32 newAuthoritySetHash,
+        uint64 epochEndBlock
     );
 
     modifier onlyGateway() {
@@ -68,11 +78,9 @@ contract VectorX {
         blockHeightToHeaderHash[_blockHeight] = _header;
         blockHeightToAuthoritySetId[_blockHeight] = _authoritySetId;
         authoritySetIdToHash[_authoritySetId] = _authoritySetHash;
-        currentAuthoritySetId = _authoritySetId;
-        currentAuthoritySetHash = _authoritySetHash;
     }
 
-    // Requests a header update and data commitment from the range (trustedBlock, requestedBlock)
+    // Requests a header update and data commitment from the range (trustedBlock + 1, requestedBlock]
     function requestHeaderRange(
         uint32 _trustedBlock,
         uint32 _requestedBlock
@@ -172,94 +180,81 @@ contract VectorX {
     }
 
     // Requests a rotate to the next authority set id, which occurs at _epochEndBlock.
-    function requestRotate(uint32 _epochEndBlock) external payable {
-        bytes32 trustedHeader = blockHeightToHeaderHash[_trustedBlock];
-        if (trustedHeader == bytes32(0)) {
-            revert("Trusted header not found");
-        }
-        uint64 authoritySetId = blockHeightToAuthoritySetId[_trustedBlock];
-        if (authoritySetId == 0) {
-            revert("Authority set ID not found");
-        }
-        bytes32 authoritySetHash = authoritySetIdToHash[authoritySetId];
-        if (authoritySetHash == bytes32(0)) {
+    function requestRotate(
+        uint32 _epochEndBlock,
+        uint64 _currentAuthoritySetId
+    ) external payable {
+        // NOTE: _epochEndBlock must be GTE the head block. Can be equal if we've already called step
+        // to the epochEndBlock.
+        require(_epochEndBlock >= head);
+
+        bytes32 currentAuthoritySetHash = authoritySetIdToHash[
+            _currentAuthoritySetId
+        ];
+        if (currentAuthoritySetHash == bytes32(0)) {
             revert("Authority set hash not found");
         }
 
-        require(_requestedBlock > _trustedBlock);
-        require(_requestedBlock - _trustedBlock <= MAX_RANGE);
-        // NOTE: this is needed to prevent a long-range attack on the light client
-        require(_requestedBlock > head);
+        bytes memory input = abi.encodePacked(
+            _currentAuthoritySetId,
+            currentAuthoritySetHash,
+            _epochEndBlock
+        );
+
+        bytes memory callbackData = abi.encodeWithSelector(
+            this.callbackHeaderRange.selector,
+            _currentAuthoritySetId,
+            currentAuthoritySetHash,
+            _epochEndBlock
+        );
 
         IFunctionGateway(gateway).requestCall{value: msg.value}(
-            headerRangeFunctionId,
-            abi.encodePacked(
-                _trustedBlock,
-                trustedHeader,
-                authoritySetId,
-                authoritySetHash,
-                _requestedBlock
-            ),
+            rotateFunctionId,
+            input,
             address(this),
-            abi.encodeWithSelector(
-                this.callbackHeaderRange.selector,
-                _trustedBlock,
-                trustedHeader,
-                authoritySetId,
-                authoritySetHash,
-                _requestedBlock
-            ),
+            callbackData,
             500000
         );
-        emit HeaderRangeRequested(
-            _trustedBlock,
-            trustedHeader,
-            authoritySetId,
-            authoritySetHash,
-            _requestedBlock
+        emit RotateRequested(
+            _currentAuthoritySetId,
+            currentAuthoritySetHash,
+            _epochEndBlock
         );
     }
 
     function callbackRotate(
-        uint32 _trustedBlock,
-        bytes32 _trustedHeader,
-        uint64 _authoritySetId,
-        bytes32 _authoritySetHash,
-        uint32 _targetBlock
+        uint64 _currentAuthoritySetId,
+        bytes32 _currentAuthoritySetHash,
+        uint32 _epochEndBlock
     ) external onlyGateway {
         bytes memory input = abi.encodePacked(
-            _trustedBlock,
-            _trustedHeader,
-            _authoritySetId,
-            _authoritySetHash,
-            _targetBlock
+            _currentAuthoritySetId,
+            _currentAuthoritySetHash,
+            _epochEndBlock
         );
 
         bytes memory output = IFunctionGateway(gateway).verifiedCall(
-            headerRangeFunctionId,
+            rotateFunctionId,
             input
         );
 
         // abi.encode matches abi.encodePacked for (bytes32, bytes32, bytes32).
-        (
-            bytes32 target_header_hash,
-            bytes32 state_root_commitment,
-            bytes32 data_root_commitment
-        ) = abi.decode(output, (bytes32, bytes32, bytes32));
+        bytes32 new_authority_set_hash = abi.decode(output, (bytes32));
 
-        blockHeightToHeaderHash[_targetBlock] = target_header_hash;
+        authoritySetIdToHash[
+            _currentAuthoritySetId + 1
+        ] = new_authority_set_hash;
+        // Note: blockHeightToAuthoritySetId[block] returns the authority set id of the next block.
+        // If the epochEndBlock is 100, and we request a step from 100 -> 147, we want the authority set of
+        // blocks 101 -> 147, so we set the authority set id of block 100 to the next authority set id.
+        blockHeightToAuthoritySetId[_epochEndBlock] =
+            _currentAuthoritySetId +
+            1;
 
-        bytes32 key = keccak256(abi.encode(_trustedBlock, _targetBlock));
-        dataRootCommitments[key] = data_root_commitment;
-        stateRootCommitments[key] = state_root_commitment;
-
-        head = _targetBlock;
-        emit HeaderRangeFulfilled(
-            _trustedBlock,
-            _targetBlock,
-            target_header_hash,
-            data_root_commitment,
-            state_root_commitment
+        emit RotateFulfilled(
+            _currentAuthoritySetId + 1,
+            new_authority_set_hash,
+            _epochEndBlock
         );
     }
 

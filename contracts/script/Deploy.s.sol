6803699ceff41ce9b42835a3d41ee46283446532
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-12-07 17:23:35 -0800

    wip

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.16;
 
 import "forge-std/Script.sol";
 import {VectorX} from "../src/VectorX.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract DeployScript is Script {
     function setUp() public {}

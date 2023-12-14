4445d8b77af8fb9f9e3f5ec9251a14f5c4a43ca5
Ratan Kaliani <ratan@succinct.xyz>
2023-12-14 12:18:29 -0800

    lint

--- circuits/step.rs
+++ circuits/step.rs
@@ -146,7 +146,7 @@ mod tests {
     use plonky2x::prelude::{DefaultBuilder, GateRegistry, HintRegistry};
 
     use super::*;
-    use crate::consts::{MAX_HEADER_SIZE, MAX_AUTHORITY_SET_SIZE, MAX_NUM_HEADERS};
+    use crate::consts::{MAX_AUTHORITY_SET_SIZE, MAX_HEADER_SIZE, MAX_NUM_HEADERS};
 
     #[test]
     #[cfg_attr(feature = "ci", ignore)]

8fc93ea73e97bcdff8dea5bfa5347424d8927b2c
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-01-16 20:05:34 -0800

    add dummy circuits

--- circuits/dummy_rotate.rs
+++ circuits/dummy_rotate.rs
@@ -1,18 +1,19 @@
-use alloy_sol_types::{sol, SolType};
 use rustx::program::Program;
 
 use crate::input::RpcDataFetcher;
 
-/// The tuple which encodes a VectorX Rotate request from the EVM.
-type RotateRequestTuple = sol! { tuple(uint64, bytes32, uint32) };
-
 #[derive(Debug, Clone)]
-struct DummyRotate;
+pub struct DummyRotate;
 impl Program for DummyRotate {
     fn run(input_bytes: Vec<u8>) -> Vec<u8> {
         // Decode the input bytes into the request tuple.
-        let (authority_set_id, authority_set_hash, epoch_end_block_number) =
-            RotateRequestTuple::abi_decode_sequence(&input_bytes, true).unwrap();
+
+        // First 8 bytes are the authority set id.
+        // Next 32 bytes are the authority set hash.
+        // Last 4 bytes are the epoch end block number.
+        let _authority_set_id = u64::from_be_bytes(input_bytes[0..8].try_into().unwrap());
+        let _authority_set_hash = input_bytes[8..40].to_vec();
+        let epoch_end_block_number = u32::from_be_bytes(input_bytes[40..44].try_into().unwrap());
 
         // Initialize tokio runtime.
         let rt = tokio::runtime::Runtime::new().unwrap();
@@ -26,5 +27,29 @@ impl Program for DummyRotate {
         });
 
         new_authority_set_hash
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rustx::program::Program;
+
+    use crate::dummy_rotate::DummyRotate;
+
+    #[test]
+    #[cfg_attr(feature = "ci", ignore)]
+    fn test_dummy_rotate() {
+        // Assert that the encoding matches.
+        let input = "0000000000000075f2da06eb7ec36f683d2908648c431a1b3f968fa5212b72cc7e8eddce8b80958d0003c6f0";
+        let input_bytes = hex::decode(input).unwrap();
+
+        // Compute the output.
+        let output = DummyRotate::run(input_bytes);
+
+        // Assert that the output matches.
+        let expected_output =
+            hex::decode("21969829db96b6cc8171290a231a150fbf4b11911eea1edb7b1d785716797a7f")
+                .unwrap();
+        assert_eq!(output, expected_output);
     }
 }
--- circuits/dummy_step.rs
+++ circuits/dummy_step.rs
@@ -1,24 +1,22 @@
-use alloy_sol_types::{sol, SolType};
 use rustx::program::Program;
 use subxt::config::Header;
 
 use crate::input::RpcDataFetcher;
 
-/// The tuple which encodes a VectorX Rotate request from the EVM.
-type StepRequestTuple = sol! { tuple(uint32, bytes32, uint64, bytes32, uint32) };
-
 #[derive(Debug, Clone)]
-struct DummyStep;
+pub struct DummyStep;
 impl Program for DummyStep {
     fn run(input_bytes: Vec<u8>) -> Vec<u8> {
-        // Decode the input bytes into the request tuple.
-        let (
-            trusted_block,
-            trusted_header_hash,
-            authority_set_id,
-            authority_set_hash,
-            target_block,
-        ) = StepRequestTuple::abi_decode_sequence(&input_bytes, true).unwrap();
+        // First 4 bytes are the trusted block number.
+        // Next 32 bytes are the trusted header hash.
+        // Next 8 bytes are the authority set id.
+        // Next 32 bytes are the authority set hash.
+        // Next 4 bytes are the target block number.
+        let trusted_block = u32::from_be_bytes(input_bytes[0..4].try_into().unwrap());
+        let _trusted_header_hash = input_bytes[4..36].to_vec();
+        let _authority_set_id = u64::from_be_bytes(input_bytes[36..44].try_into().unwrap());
+        let _authority_set_hash = input_bytes[44..76].to_vec();
+        let target_block = u32::from_be_bytes(input_bytes[76..80].try_into().unwrap());
 
         // Initialize tokio runtime.
         let rt = tokio::runtime::Runtime::new().unwrap();
@@ -45,5 +43,28 @@ impl Program for DummyStep {
             .chain(result.1)
             .chain(result.2)
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rustx::program::Program;
+
+    use crate::dummy_step::DummyStep;
+
+    #[test]
+    #[cfg_attr(feature = "ci", ignore)]
+    fn test_dummy_step() {
+        // Assert that the encoding matches.
+        let input = "0003c18695f303b01e4834da35e5fdc3971fe297d1b48feb0c3f330491639136a6ada5980000000000000075f2da06eb7ec36f683d2908648c431a1b3f968fa5212b72cc7e8eddce8b80958d0003c23a";
+        let input_bytes = hex::decode(input).unwrap();
+
+        // Compute the output.
+        let output = DummyStep::run(input_bytes);
+
+        let expected_output =
+            hex::decode("3aaa82535ce715acb251047c280d5492d1330c41fe24c9841db508ba961dce464cb5c2a82cc64e401ac01ba85c471fe1dab4fe4baf7a96c306d4e94dcb428f47ead156d58c77adfa928845f048b50fd92e871776dfa76ed2f98c6ef823aa7a2d")
+                .unwrap();
+        assert_eq!(output, expected_output);
     }
 }

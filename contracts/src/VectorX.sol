df9ce4ceb54baab181a6f80076aa7943d311d6b0
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-04-03 16:09:49 -0700

    fix

--- contracts/src/VectorX.sol
+++ contracts/src/VectorX.sol
@@ -296,7 +296,13 @@ contract VectorX is IVectorX, TimelockedUpgradeable {
             _currentAuthoritySetId
         );
 
-        ISuccinctGateway(gateway).requestCall{value: msg.value}(500000);
+        ISuccinctGateway(gateway).requestCall{value: msg.value}(
+            rotateFunctionId,
+            input,
+            address(this),
+            data,
+            500000
+        );
 
         emit RotateRequested(_currentAuthoritySetId, currentAuthoritySetHash);
     }

63a6e0fc177084eb62a687a5bc24275e5875d4d3
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-05-01 17:38:51 -0700

    fix: vectorx contracts

--- contracts/src/VectorX.sol
+++ contracts/src/VectorX.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.25;
 
 import {IVectorX} from "./interfaces/IVectorX.sol";
 import {TimelockedUpgradeable} from "@succinctx/upgrades/TimelockedUpgradeable.sol";
--- contracts/src/interfaces/IVectorX.sol
+++ contracts/src/interfaces/IVectorX.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.25;
 
 interface IVectorX {
     /// @notice Emits event with the inputs of a header range request.
